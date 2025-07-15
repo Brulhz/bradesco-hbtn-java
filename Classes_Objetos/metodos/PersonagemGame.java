325e0dd643dc6d5e0d9478ec90a746f83a717647
Bruno Melo <brumesi@hotmail.com>
2025-07-15 20:44:28 -0300

    adjust

--- Classes_Objetos/metodos/PersonagemGame.java
+++ Classes_Objetos/metodos/PersonagemGame.java
@@ -3,27 +3,22 @@ public class PersonagemGame {
     private int saudeAtual;
     private String nome;
 
-    // Getter para saudeAtual
     public int getSaudeAtual() {
         return saudeAtual;
     }
 
-    // Setter para saudeAtual
     public void setSaudeAtual(int saudeAtual) {
         this.saudeAtual = saudeAtual;
     }
 
-    // Getter para nome
     public String getNome() {
         return nome;
     }
 
-    // Setter para nome
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    // Método tomarDano
     public void tomarDano(int quantidadeDeDano) {
         saudeAtual -= quantidadeDeDano;
         if (saudeAtual < 0) {
@@ -31,7 +26,6 @@ public class PersonagemGame {
         }
     }
 
-    // Método receberCura
     public void receberCura(int quantidadeDeCura) {
         saudeAtual += quantidadeDeCura;
         if (saudeAtual > 100) {
