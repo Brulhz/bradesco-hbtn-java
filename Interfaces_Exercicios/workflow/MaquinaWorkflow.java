4ebadf11d96e84acc1e6e3ac868c8e61e0c5ca2f
Bruno Melo <brumesi@hotmail.com>
2025-08-05 18:43:30 -0300

    exercicio 3

--- Interfaces_Exercicios/workflow/MaquinaWorkflow.java
+++
@@ -1,7 +0,0 @@
-public class MaquinaWorkflow {
-    public void executar(Workflow workflow) {
-        for (atividades.Atividade atividade : workflow.getAtividades()) {
-            atividade.executar();
-        }
-    }
-}
