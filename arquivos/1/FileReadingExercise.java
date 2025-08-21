85c330dfeb84675daa5f260d754e636a85a71d71
Bruno de Melo Silva <brumesi@hotmail.com>
2025-08-21 20:30:24 -0300

    new exercise1

--- arquivos/1/FileReadingExercise.java
+++ arquivos/1/FileReadingExercise.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class FileReadingExercise {
     public static void main(String[] args) {
@@ -9,8 +10,9 @@ public class FileReadingExercise {
 
         System.out.println("Conteúdo do arquivo '" + fileName + "':\n");
 
+
         try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(new FileInputStream(fileName), "UTF-8"))) {
+                new InputStreamReader(new FileInputStream(fileName), StandardCharsets.UTF_8))) {
 
             String linha;
             while ((linha = reader.readLine()) != null) {
