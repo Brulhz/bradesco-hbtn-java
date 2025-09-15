40fb8467c6e04d5d1c258f43a51d11fb262dc0d6
Bruno de Melo Silva <brumesi@hotmail.com>
2025-09-15 20:45:53 -0300

    teste

--- Streams_Collectors_Optional/livros/Consulta.java
+++
@@ -1,7 +0,0 @@
-class Consulta {
-    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
-        return pedido.getProdutos().stream()
-                .filter(p -> p.getCategoria() == CategoriaProduto.LIVRO)
-                .collect(Collectors.toList());
-    }
-}
