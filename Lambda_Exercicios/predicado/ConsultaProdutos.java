07c47c4498fd41c940fff38865824f0f950c80fb
Bruno de Melo Silva <brumesi@hotmail.com>
2025-09-15 20:24:56 -0300

    teste

--- Lambda_Exercicios/predicado/ConsultaProdutos.java
+++ Lambda_Exercicios/predicado/ConsultaProdutos.java
@@ -1,10 +1,11 @@
 import java.util.*;
+import java.util.function.Predicate;
 
 public class ConsultaProdutos {
-    public static List<Produto> filtrar(List<Produto> produtos, CriterioFiltro criterio) {
+    public static List<Produto> filtrar(List<Produto> produtos, Predicate<Produto> criterio) {
         List<Produto> resultado = new ArrayList<>();
         for (Produto p : produtos) {
-            if (criterio.testar(p)) {
+            if (criterio.test(p)) {
                 resultado.add(p);
             }
         }
