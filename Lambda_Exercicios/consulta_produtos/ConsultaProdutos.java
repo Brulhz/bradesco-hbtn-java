a7fc747b4cd9491d0174345148a4b43d821a8f7d
Bruno de Melo Silva <brumesi@hotmail.com>
2025-09-15 20:19:22 -0300

    teste

--- Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
+++ Lambda_Exercicios/consulta_produtos/ConsultaProdutos.java
@@ -10,16 +10,4 @@ public class ConsultaProdutos {
         }
         return resultado;
     }
-}import java.util.*;
-
-public class ConsultaProdutos {
-    public static List<Produto> filtrar(List<Produto> produtos, CriterioFiltro criterio) {
-        List<Produto> resultado = new ArrayList<>();
-        for (Produto p : produtos) {
-            if (criterio.testar(p)) {
-                resultado.add(p);
-            }
-        }
-        return resultado;
-    }
 }
