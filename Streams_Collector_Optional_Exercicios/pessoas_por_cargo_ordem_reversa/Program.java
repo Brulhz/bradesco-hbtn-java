4f10d718f6f0c6cab4b64d936d3ca3efdecbe4f9
Bruno de Melo Silva <brumesi@hotmail.com>
2025-09-21 10:29:50 -0300

    exercise

--- Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Program.java
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Program.java
@@ -13,19 +13,15 @@ public class Program {
         Pessoa pessoa9 = new Pessoa(9, "Andrei Melendez", "Analista QA", 42, 5330);
         Pessoa pessoa10 = new Pessoa(10, "Branden Irwin", "Analista QA", 21, 2800);
 
-        List<Pessoa> todasPessoas = List.of(
-                pessoa1, pessoa2, pessoa3, pessoa4, pessoa5,
-                pessoa6, pessoa7, pessoa8, pessoa9, pessoa10
+        List<Pessoa> todasPessoas = List.of(pessoa1, pessoa2, pessoa3, pessoa4, pessoa5,
+                pessoa6, pessoa7, pessoa8, pessoa9, pessoa10);
+
+        TreeMap<String, TreeSet<Pessoa>> pessoas1 = ConsultaPessoas.obterPessoasAgrupadasPorCargoEmOrdemReversa(todasPessoas);
+        TreeMap<String, TreeSet<Pessoa>> pessoas2 = ConsultaPessoas.obterPessoasAgrupadasPorCargoEmOrdemReversa(
+                List.of(pessoa1, pessoa2, pessoa3, pessoa4, pessoa9, pessoa10)
         );
-
-        Map<String, List<Pessoa>> pessoas1 = ConsultaPessoas.obterPessoasPorCargoAcimaDe40anos(todasPessoas);
-
-        Map<String, List<Pessoa>> pessoas2 = ConsultaPessoas.obterPessoasPorCargoAcimaDe40anos(
-                List.of(pessoa1, pessoa4, pessoa10)
-        );
-
-        Map<String, List<Pessoa>> pessoas3 = ConsultaPessoas.obterPessoasPorCargoAcimaDe40anos(
-                List.of(pessoa4, pessoa9)
+        TreeMap<String, TreeSet<Pessoa>> pessoas3 = ConsultaPessoas.obterPessoasAgrupadasPorCargoEmOrdemReversa(
+                List.of(pessoa1, pessoa3, pessoa4, pessoa5, pessoa9)
         );
 
         System.out.println(pessoas1);
